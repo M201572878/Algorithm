9cfea3b4a380d98fd4f3f5a1aada244d34caa85b
hyhyx6125 <987588074@qq.com>
2018-04-16 02:41:05 +0800

    #添加libevent

--- VisualStudio/TestCpp/main.cpp
+++ VisualStudio/TestCpp/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include "TestPython.hpp"
-#include "TestDiffPatch.hpp"
-#include "PythonUtil.hpp"
+//#include "TestPython.hpp"
+//#include "TestDiffPatch.hpp"
+//#include "PythonUtil.hpp"
+#include "event2/event.h"
+#include "event.h"
 
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"libevent.lib")
+#pragma comment(lib,"libevent_core.lib")
+#pragma comment(lib,"libevent_extras.lib")
+#pragma comment(lib,"WSock32.lib")
 using namespace std;
 
 int main()
@@ -10,12 +17,15 @@ int main()
 	//TestPython::run();
 
 	//TestDiffPatch::RunWstring();
-	string str = "1";
+	/*string str = "1";
 	cout << str.length() << " " << str.size() << endl;
 
 	string inStr("sssssss09765653453457679885674ÖÐÎÄs"),outStr;
 	PythonUtil::Instance().Compress(inStr, outStr);
-	cout << outStr.length()<<" "<<outStr << endl;
+	cout << outStr.length()<<" "<<outStr << endl;*/
+
+	const char *version = event_get_version();
+	printf("%s\n", version);
 	
 	getchar();
 	return 0;
